316943546dfc33ed4f4a93e160dfa8a2af87a7c3
srinathponnamudi-reed <srinath.ponnamudi@reed.com>
2024-04-11 11:05:19 +0100

    add other route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadChildren: () => import('./routes/dashboard/routes').then(mod => mod.dashboardRoutes)
   },
   {
+    path: 'other',
+    loadChildren: () => import('./routes/other/routes').then(mod => mod.otherRoutes)
+  },
+  {
     path: '**',
     redirectTo: '/dashboard'
   }
---
+++ src/app/routes/other/routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router'
+import { OtherComponent } from './components/other/other.component'
+ 
+export const otherRoutes: Route[] = [
+  {
+    path: '',
+    component: OtherComponent
+  }
+]
