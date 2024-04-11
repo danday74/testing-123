e77ac7f338610955cfb4b4139970553cdef581d8
Daniel Lewis <daniel.lewis@reed.com>
2024-04-11 13:52:16 +0100

    Added imports

--- src/app/routes/dashboard/components/navbar/navbar.component.ts
+++ src/app/routes/dashboard/components/navbar/navbar.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatFormField } from '@angular/material/form-field'
+import { MatOption, MatSelect } from '@angular/material/select'
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatSelect,
+    MatOption
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
