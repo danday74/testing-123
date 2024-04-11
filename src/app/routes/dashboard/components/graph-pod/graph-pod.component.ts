6dbc61ae92e53ecb37d5500d0c177eb9314efda3
Daniel Lewis <daniel.lewis@reed.com>
2024-04-11 15:07:09 +0100

    Working drag

--- src/app/routes/dashboard/components/graph-pod/graph-pod.component.ts
+++ src/app/routes/dashboard/components/graph-pod/graph-pod.component.ts
@@ -1,10 +1,13 @@
 import { Component, input, InputSignal } from '@angular/core'
 import { IBasicPod, IGraphPod, IPod } from '../../../../interfaces/i-pod'
+import { CdkDrag } from '@angular/cdk/drag-drop'
 
 @Component({
   selector: 'app-graph-pod',
   standalone: true,
-  imports: [],
+  imports: [
+    CdkDrag
+  ],
   templateUrl: './graph-pod.component.html',
   styleUrl: './graph-pod.component.scss'
 })
